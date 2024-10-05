e932463607a64636e21ed3d8dcda70e43f389e50
Kumuthu Piyasoma <kumuthu@geveo.com>
2024-10-06 01:17:26 +0530

    Installed and implemented half gauge dependency

---
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.testapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.ekn.gruzer.gaugelibrary.Range;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.ekn.gruzer.gaugelibrary.HalfGauge;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        HalfGauge halfGauge = findViewById(R.id.half_gauge);
+
+        Range range = new Range();
+        range.setColor(Color.parseColor("#ce0000"));
+        range.setFrom(0.0);
+        range.setTo(50.0);
+
+        Range range2 = new Range();
+        range2.setColor(Color.parseColor("#E3E500"));
+        range2.setFrom(50.0);
+        range2.setTo(100.0);
+
+        Range range3 = new Range();
+        range3.setColor(Color.parseColor("#00b20b"));
+        range3.setFrom(100.0);
+        range3.setTo(150.0);
+
+        //add color ranges to gauge
+        halfGauge.addRange(range);
+        halfGauge.addRange(range2);
+        halfGauge.addRange(range3);
+
+        //set min max and current value
+        halfGauge.setMinValue(0.0);
+        halfGauge.setMaxValue(150.0);
+        halfGauge.setValue(0.0);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
